878d5ec95fe78898547dc105d7ea776e1cd2bac6
Edwin Narvaez <davidnarvaezmiranda97@gmail.com>
2021-06-15 22:59:01 -0500

    First commit, loading the main libraries

---
+++ app.js
@@ -0,0 +1,93 @@
+window.saveDataACrossSessions = true;
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        selected: "",
+        photos: ["amazon", "ebay", "fb", "google", "laarbox", "medium", "twitter", "udemy"],
+        coor: [],
+
+    },
+    methods: {
+        startRecord() {
+            this.coor = []
+            setTimeout(() => {
+                this.download_csv(this.selected)
+            }, 10000);
+            this.coor = []
+
+        },
+        download_csv(name) {
+            var csv = 'X,Y\n';
+            this.coor.forEach(function(row) {
+                csv += row.join(',');
+                csv += "\n";
+            });
+
+            console.log(csv);
+            var hiddenElement = document.createElement('a');
+            hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+            hiddenElement.target = '_blank';
+            hiddenElement.download = name + '.csv';
+            hiddenElement.click();
+        }
+    }
+})
+
+
+//let datos = []
+// webgazer.setGazeListener((data, timestamps) => {
+//         console.log(data, timestamps)
+//         try {
+//             datos.push([data.x, data.y])
+//         } catch (e) {
+//             console.log("configurando");
+//         }
+
+//     })
+//     .begin()
+
+// fotos = ["amazon", "ebay", "fb", "google", "laarbox", "medium", "twitter", "udemy"]
+
+
+// let botton = document.querySelector("#btn");
+// let contador = 0
+
+// function download_csv(name) {
+//     var csv = 'X,Y\n';
+//     datos.forEach(function(row) {
+//         csv += row.join(',');
+//         csv += "\n";
+//     });
+
+//     console.log(csv);
+//     var hiddenElement = document.createElement('a');
+//     hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+//     hiddenElement.target = '_blank';
+//     hiddenElement.download = name + '.csv';
+//     hiddenElement.click();
+// }
+// document.querySelector("#btn").addEventListener("click", () => {
+//     datos = []
+//     setTimeout(function() {
+//         if (contador >= fotos.length) {
+//             contador = 0
+//         }
+//         let img = document.querySelector("#foto");
+//         img.src = "/images/" + fotos[contador] + ".JPG";
+//         download_csv(fotos[contador])
+//         datos = []
+//         contador = contador + 1;
+//     }, 10000);
+// })
+
+
+
+// fotos.forEach(element => {
+//     let select = document.querySelector("#select")
+//     let option = document.createElement('option');
+//     option.value = element
+//     select.appendChild(option)
+
+
+// });
